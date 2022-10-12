0e4929d5302514820e7d412d4ad5bab0bfb63c36
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2022-10-12 19:06:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import {React,useState} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [alternate,setAlternate]=useState();
+  const ChangeHandler=()=>{
+    if(alternate===false){
+      setAlternate(true)
+  }
+    else{
+      setAlternate(false)
+    }
+  }
   
   return (
     <div id="main">
-
+    <h1 id="'marco-polo'">{alternate ? "Polo" : "Marco"}</h1>
+    <button id="marco-polo-toggler" onClick="ChangeHandler">{alternate ? "Marco" : "Polo"}</button>
     </div>
   )
 }
